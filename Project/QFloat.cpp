6750cdf88c415a96033af8fddc10e33ae6265cfc
thinhnguyenphuc555 <thinhnguyenphuc@yahoo.com>
2019-11-27 19:40:09 +0700

    CompleteQFloat

--- Project/QFloat.cpp
+++ Project/QFloat.cpp
@@ -79,12 +79,13 @@ std::string calBeforeComma(std::string bin_tmp)
 
 std::string FloatBinToDec(std::string bin)
 {
-	int res = 1;
+	int res = 0;
 	string tmp;
 	string result;
 	int BitZero = FindBitZero(bin);
-	int IndexNum = FindIndexNum(bin,res);
-	if (SpecialCase(bin, IndexNum, res+IndexNum))
+	int IndexNum = FindIndexNum(bin, res);
+	string res_ = pow_2_n(res);
+	if (SpecialCase(bin, IndexNum, res + IndexNum))
 	{
 		return "\nEnd";
 	}
@@ -92,9 +93,9 @@ std::string FloatBinToDec(std::string bin)
 	{
 		tmp = addFloatingPoint(bin, IndexNum);
 		tmp = addOneInStart(tmp, BitZero);
-		result = toDecString(bin, AfterComma(tmp), BeforeComma(tmp));
+		result = MultiplyNumberString((toDecString(bin, AfterComma(tmp), BeforeComma(tmp))), res_);
 	}
-		return result;
+	return result;
 }
 
 int calIndexNum(std::string bin_temp)
@@ -110,11 +111,11 @@ int calIndexNum(std::string bin_temp)
 
 std::string calAfterComma(std::string bin_tmp)
 {
-	string result;
+	string result = { '0' };
 	for (int i = 0; i < bin_tmp.length(); i++)
 	{
 		if (bin_tmp[i] == '1')
-			result = AddNumberString(result, pow_2_n(-i));
+			result = AddNumberFloatString(result, pow_2_n(-i - 1));
 	}
 	return result;
 }
@@ -138,6 +139,118 @@ int FindIndexNum(std::string bin, int& res)
 	else return result;
 }
 
+int CharToIntNum(char n)
+{
+	int tmp = int(n);
+	switch (tmp)
+	{
+	case 48:
+		return 0;
+	case 49:
+		return 1;
+	case 50:
+		return 2;
+	case 51:
+		return 3;
+	case 52:
+		return 4;
+	case 53:
+		return 5;
+	case 54:
+		return 6;
+	case 55:
+		return 7;
+	case 56:
+		return 8;
+	case 57:
+		return 9;
+	}
+}
+char IntToCharNum(int n)
+{
+	switch (n)
+	{
+	case 0:
+		return 48;
+	case 1:
+		return 49;
+	case 2:
+		return 50;
+	case 3:
+		return 51;
+	case 4:
+		return 52;
+	case 5:
+		return 53;
+	case 6:
+		return 54;
+	case 7:
+		return 55;
+	case 8:
+		return 56;
+	case 9:
+		return 57;
+	}
+}
+
+std::string AddNumberFloatString(std::string a, std::string b)
+{
+	string tmp1;
+	int res = 0;
+	if (a.length() >= b.length())
+	{
+		for (int i = 0; i < a.length(); i++)
+		{
+			tmp1.push_back('0');
+		}
+		for (int i = a.length() - 1; i > b.length() - 1; i--)
+		{
+			tmp1[i] = a[i];
+		}
+		tmp1[1] = '.';
+		for (int i = b.length() - 1; i > 1; i--)
+		{
+			int tmp = CharToIntNum(a[i]) + CharToIntNum(b[i]);
+			if (tmp + res >= 10)
+			{
+				tmp1[i] = (IntToCharNum((tmp + res) % 10));
+				res = tmp / 10;
+			}
+			else
+			{
+				tmp1[i] = IntToCharNum(tmp + res);
+				res = 0;
+			}
+		}
+	}
+	else
+	{
+		for (int i = 0; i < b.length(); i++)
+		{
+			tmp1.push_back('0');
+		}
+		for (int i = b.length() - 1; i > a.length() - 1; i--)
+		{
+			tmp1[i] = b[i];
+		}
+		tmp1[1] = '.';
+		for (int i = a.length() - 1; i > 1; i--)
+		{
+			int tmp = CharToIntNum(a[i]) + CharToIntNum(b[i]);
+			if (tmp + res >= 10)
+			{
+				tmp1[i] = (IntToCharNum((tmp + res) % 10));
+				res = tmp / 10;
+			}
+			else
+			{
+				tmp1[i] = IntToCharNum(tmp + res);
+				res = 0;
+			}
+		}
+	}
+	return tmp1;
+}
 std::string addFloatingPoint(std::string bin, int IndexNum)
 {
 	string tmp;
@@ -217,7 +330,7 @@ std::string AfterComma(std::string bin_tmp)
 	{
 		if (bin_tmp[i] == '.')
 		{
-			pivot = i;
+			pivot = i + 1;
 			break;
 		}
 	}
@@ -237,13 +350,13 @@ std::string toDecString(std::string bin, std::string afterComma, std::string Bef
 	{
 		result.push_back(BeforeComma[i]);
 	}
-	for (int i = 1; i < tmp.length(); i++)
+	for (int i = 1; i < afterComma.length(); i++)
 	{
-		result.push_back(tmp[i]);
+		result.push_back(afterComma[i]);
 	}
 	return result;
 }
-bool SpecialCase(std::string bin , int IndexNum, int res)
+bool SpecialCase(std::string bin, int IndexNum, int res)
 {
 	if (Zero(bin) && IndexNum == 0)
 	{
@@ -269,8 +382,8 @@ bool SpecialCase(std::string bin , int IndexNum, int res)
 }
 bool Zero(std::string bin_tmp)
 {
-	int key=0;
-	for (int i= 16; i < bin_tmp.length(); i++)
+	int key = 0;
+	for (int i = 16; i < bin_tmp.length(); i++)
 	{
 		if (key == 0 && i == 127)
 			return true;
@@ -285,13 +398,32 @@ bool Zero(std::string bin_tmp)
 ////////////////////////////////////////////////////////
 std::string FloatDecToBin(std::string bin)
 {
-	string BeforeComma, AfterComma, tmp;
-	BeforeComma = CutBefore(bin);
-	AfterComma = CutAfter(bin);
-	return BeforeComma;
+	string BeforeComma, AfterComma, tmp, Index, sign;
+	int IndexN;
+	if (SpecialCaseDecToBin(bin))
+	{
+		return "\nEnd";
+	}
+	else
+	{
+		BeforeComma = ConvertBefore(CutBefore(bin));
+		AfterComma = ConvertAfter(CutAfter(bin));
+		tmp = Standardized(BeforeComma, AfterComma, IndexN);
+		Index = IndexNum(IndexN);
+		sign = Sign(bin);
+		return toBinString(sign, Index, tmp);
+	}
 }
 
-
+bool SpecialCaseDecToBin(std::string bin)
+{
+	if (bin[0] == '0')
+	{
+		cout << "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+		return true;
+	}
+	return false;
+}
 
 std::string CutBefore(std::string bin)
 {
@@ -310,11 +442,17 @@ std::string CutBefore(std::string bin)
 std::string CutAfter(std::string bin)
 {
 	string tmp;
+	tmp.push_back('0');
+	tmp.push_back('.');
 	for (int i = 0; i < bin.length(); i++)
 	{
 		if (bin[i] == '.')
 		{
-			tmp.push_back(bin[i]);
+			for (int j = i + 1; j < bin.length(); j++)
+			{
+				tmp.push_back(bin[j]);
+			}
+			break;
 		}
 	}
 	return tmp;
@@ -322,7 +460,20 @@ std::string CutAfter(std::string bin)
 
 std::string ConvertBefore(std::string before)
 {
-	return BinToDec(before);
+	string tmp, result;
+	tmp = DecToBin(before);;
+	for (int i = 0; i < tmp.length(); i++)
+	{
+		if (tmp[i] == '1')
+		{
+			for (int j = i; j < tmp.length(); j++)
+			{
+				result.push_back(tmp[j]);
+			}
+			break;
+		}
+	}
+	return result;
 }
 
 std::string cleanFloat(std::string num) {
@@ -330,7 +481,7 @@ std::string cleanFloat(std::string num) {
 	int rzero = num.find_last_not_of('0');
 	int dot = num.find_first_not_of('.');
 
-	num.erase(rzero+1, num.size());
+	num.erase(rzero + 1, num.size());
 	if (lzero < dot - 1) {
 		num = clean(num);
 	}
@@ -354,21 +505,21 @@ std::string MultiplyNumberString(std::string a, std::string b) {
 		}
 	}
 
-	int own { 0 };
+	int own{ 0 };
 	bool as = (a.find_first_of('.') != std::string::npos);
 	bool bs = (b.find_first_of('.') != std::string::npos);
-	
+
 	if (as) {
 		a = cleanFloat(a);
 		int dot = a.find_first_of(".");
-		own += (a.size()-dot-1);
+		own += (a.size() - dot - 1);
 		a.erase(dot, 1);
 		a.erase(0, a.find_first_not_of('0'));
 	}
 	if (bs) {
 		b = cleanFloat(b);
 		int dot = b.find_first_of(".");
-		own += (b.size()-dot-1);
+		own += (b.size() - dot - 1);
 		b.erase(dot, 1);
 		b.erase(0, b.find_first_not_of('0'));
 	}
@@ -406,7 +557,7 @@ std::string MultiplyNumberString(std::string a, std::string b) {
 		Result.insert(0, "0.");
 	}
 	else {
-		Result.insert(Result.size()-own, ".");
+		Result.insert(Result.size() - own, ".");
 	}
 
 	// while (Result[Result.size()-1] == '0' && own > 0) {
@@ -424,4 +575,145 @@ std::string MultiplyNumberString(std::string a, std::string b) {
 
 	return buf + Result;
 }
-//std::string ConvertAfter(std::string after)
+
+std::string ConvertAfter(std::string after)
+{
+	string result;
+	string tmp = after;
+	for (int i = 0; i < 112; i++)
+	{
+		tmp = MultiplyNumberString(tmp, "2");
+		if (tmp[0] == '1')
+		{
+			result.push_back('1');
+			tmp[0] = '0';
+		}
+		else
+		{
+			result.push_back('0');
+		}
+	}
+	return result;
+}
+
+std::string Standardized(std::string before, std::string after, int& IndexNum)
+{
+	string result, tmp1, tmp2;
+	for (int i = 0; i < before.length(); i++)
+	{
+		tmp1.push_back(before[i]);
+	}
+	tmp1.push_back('.');
+	for (int i = 0; i < after.length(); i++)
+	{
+		tmp1.push_back(after[i]);
+	}
+	int tmp = before.length();
+	if (before[0] == NULL)
+	{
+		tmp2.push_back('0');
+		for (int i = 0; i < 113; i++)
+		{
+			tmp2.push_back(tmp1[i]);
+		}
+		for (int i = 0; i < 113; i++)
+		{
+			if (tmp2[i] == '.')
+			{
+				for (int j = i + 1; i < tmp2.length(); j++)
+				{
+					if (tmp2[j] == '1')
+					{
+						IndexNum = -(j - i);
+						break;
+					}
+				}
+				break;
+			}
+		}
+	}
+	else
+	{
+		for (int i = 0; i < 113; i++)
+		{
+			tmp2.push_back(tmp1[i]);
+		}
+		for (int i = 0; i < 113; i++)
+		{
+			if (tmp2[i] == '.')
+			{
+				IndexNum = i - 1;
+				break;
+			}
+		}
+	}
+	for (int i = 0; i < 113; i++)
+	{
+		if (tmp2[i] == '1')
+		{
+			for (int j = i + 1; j < 113 - i; j++)
+			{
+				if (tmp2[j] != '.')
+				{
+					result.push_back(tmp2[j]);
+				}
+			}
+			break;
+		}
+	}
+	return result;
+}
+
+std::string IndexNum(int IndexNum)
+{
+	string result;
+	int tmp = pow(2, 14) + IndexNum - 1;
+	int i = 0;
+	int binaryNum[15];
+	while (tmp > 0)
+	{
+		binaryNum[i] = tmp % 2;
+		tmp = tmp / 2;
+		i++;
+	}
+	for (int k = 14; k >= 0; k--)
+	{
+		if (binaryNum[k] == 1)
+		{
+			result.push_back('1');
+		}
+		else
+			result.push_back('0');
+
+	}
+	return result;
+}
+
+std::string Sign(std::string bin)
+{
+	if (bin[0] == '-')
+		return "1";
+	else return "0";
+}
+
+std::string toBinString(std::string sign, std::string index, std::string significand)
+{
+	string result;
+	result.push_back(sign[0]);
+	for (int i = 0; i < 15; i++)
+	{
+		result.push_back(index[i]);
+	}
+	for (int i = 0; i < significand.length(); i++)
+	{
+		result.push_back(significand[i]);
+	}
+	if (result.length() < 132)
+	{
+		for (int i = result.length(); i < 132; i++)
+		{
+			result.push_back('0');
+		}
+	}
+	return result;
+}
--- Project/QFloat.h
+++ Project/QFloat.h
@@ -21,11 +21,19 @@ std::string FloatDecToBin(std::string bin);
 ///////////////////////////////////////////////////
 std::string calBeforeComma(std::string bin_tmp);
 
+bool SpecialCaseDecToBin(std::string bin);
+
 std::string calAfterComma(std::string bin_tmp);
 
 int FindBitZero(std::string bin);
 
-int FindIndexNum(std::string bin, int &res);
+int FindIndexNum(std::string bin, int& res);
+
+int CharToIntNum(char n);
+
+char IntToCharNum(int n);
+
+std::string AddNumberFloatString(std::string a, std::string b);
 
 std::string addFloatingPoint(std::string bin, int IndexNum);
 
@@ -34,6 +42,8 @@ std::string addOneInStart(std::string bin_tmp, int BitZero);
 std::string BeforeComma(std::string bin_tmp);
 
 std::string AfterComma(std::string bin_tmp);
+
+std::string AfterCommaDec(std::string bin_tmp);
 
 std::string toDecString(std::string bin, std::string afterComma, std::string BeforeComma);
 
@@ -52,3 +62,11 @@ std::string ConvertAfter(std::string after);
 std::string cleanFloat(std::string num);
 
 std::string MultiplyNumberString(std::string a, std::string b);
+
+std::string Standardized(std::string before, std::string after, int& IndexNum);
+
+std::string IndexNum(int IndexNum);
+
+std::string Sign(std::string bin);
+
+std::string toBinString(std::string sign, std::string index, std::string significand);
