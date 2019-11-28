7ca2cfb89d137d22ba63af8f5ef1a670eb3ff8d4
Nam Huynh Nhat <nnhuynh306@gmail.com>
2019-11-28 23:26:50 +0700

    comment functions class Console

--- Project/Console.h
+++ Project/Console.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include "Qfloat.h"
 #include "QInt.h"
@@ -14,18 +14,25 @@ public:
 private:
     bool modeQInt;
 
+	//chạy chương trình chế độ QInt
     void runQIntMode();
-
+	
+	//chạy chương trình chế độ QFloat
     void runQFloatMode();
 
+	//Yêu cầu chuyển đổi hoặc tính toán
     int QIntMenu();
 
+	//chuyển đổi base
     void QIntConverter();
 
+	//tính toán số học
     void QIntArithmetic();
 
+	//yêu cầu đổi bin to dec hay dec to bin
     int QFloatMenu();
 
+	//xử lý yêu cầu của người dùng, tính toán kết quả
     std::string executeCommandLine(std::vector<string> &tokens);
 
     
